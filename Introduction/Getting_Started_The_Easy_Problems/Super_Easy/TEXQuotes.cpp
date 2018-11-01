2afcd09ea9b572ced59b60c730cd81e124606b95
Rone Filho <rone_filho@hotmail.com>
2018-11-01 18:18:25 -0300

    vs sync

--- Introduction/Getting_Started_The_Easy_Problems/Super_Easy/TEXQuotes.cpp
+++ Introduction/Getting_Started_The_Easy_Problems/Super_Easy/TEXQuotes.cpp
@@ -1,40 +1,44 @@
 #include <iostream>
+#include <cstdio>
 #include <string>
 
 using namespace std;
 
 int main()
 {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
 	string entry, output;
 	bool first = true;
 
-	while ( getline(cin, entry)) {
-
-		entry = "";
+	while (getline(cin, entry))
+	{
 		output = "";
 
-		for (int i = 0; i < entry.size(); i++) {
+		for (int i = 0; i < entry.size(); i++)
+		{
 
-			if (entry[i] == '"') {
+			if (entry[i] == '"')
+			{
 
-				if (first) {
+				if (first)
+				{
 					output += "``";
 					first = false;
 				}
-				else {
+				else
+				{
 					output += "''";
 					first = true;
 				}
-
 			}
-			else {
+			else
+			{
 				output += entry[i];
 			}
-
 		}
 
 		printf("%s\n", output.c_str());
-
 	}
-
 }
